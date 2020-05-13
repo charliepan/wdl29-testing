26c6b9615f70609ac9826a3e69926f124ad6329a
mattcbodily <mattcbodily@gmail.com>
2020-05-13 08:28:35 -0600

    instructor branch finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './Components/Header';
+import Todos from './Components/Todos';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Todos />
     </div>
   );
 }
---
+++ src/_tests_/functions.test.js
@@ -0,0 +1,30 @@
+const {sum, sayHello} = require('../functions');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).not.toBeNaN()
+})
+
+test('sayHello says hello', () => {
+    expect(sayHello()).toBe('hello')
+})
+
+test('object assignment', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+})
+
+test('expect true to be truthy', () => {
+    expect(true).toBeTruthy()
+})
+
+test('expect false to be falsy', () => {
+    expect(false).not.toBeTruthy()
+})
+
+let names = ['John', 'Ariel', 'Zach'];
+
+test('names contains Ariel', () => {
+    expect(names).toContain('Ariel')
+})
